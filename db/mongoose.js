0592af0d18814b0e5921401cdc677603ddc43817
Dimags500 <73039506+Dimags500@users.noreply.github.com>
2022-07-16 18:02:20 +0300

    fix 5

--- db/mongoose.js
+++ db/mongoose.js
@@ -5,9 +5,7 @@ dotenv.config();
 
 const URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.nezyc.mongodb.net/?retryWrites=true&w=majority`;
 
-const localURl =
-  "mongodb+srv://nick:lq0qSc6xuS8HjkbW@cluster0.nezyc.mongodb.net/?retryWrites=true&w=majority";
 mongoose
-  .connect(localURl)
+  .connect(URL)
   .then(() => console.log("DB connected"))
   .catch((error) => console.log(error));
